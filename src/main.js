9fad65474a55e1074cee0f57b950080079add370
hiens <huuhienvip@gmail.com>
2021-04-20 14:24:35 +0700

    What the hell?

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import { createStore } from 'vuex'
-import App from './app.vue'
+import App from './App.vue'
 import { routes } from './routes.js'
 import store from "./store/index"
 
